b15aed1836e268e8163c918fe19b5c74b723c5de
shreya079 <gshreyareddy94@gmail.com>
2016-02-14 16:42:25 +0530

    Update remove.java

--- remove.java
+++ remove.java
@@ -5,8 +5,8 @@ public static String Remove(String s)
 	s=" ";
 	for(int i=0;i<vec.size();i++)
 	{
-											s=vec.get(i).replaceAll("my|name|is| a|an|of|the|or|of|										for|in|as|at","").trim();
-											}
-											return s;
+	s=vec.get(i).replaceAll("my|name|is| a|an|of|the|or|of|for|in|as|at","").trim();
+	}
+	return s;
 															
-																}
+	}
