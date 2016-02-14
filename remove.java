cb4e760e752f4f8bcd1878b8a7206f7ec4d1bd6c
shreya079 <gshreyareddy94@gmail.com>
2016-02-14 16:40:26 +0530

    method that removes useless words

---
+++ remove.java
@@ -0,0 +1,12 @@
+public static String Remove(String s)
+	{
+	Vector<String> vec= new Vector<String>();
+	vec.add(s);
+	s=" ";
+	for(int i=0;i<vec.size();i++)
+	{
+											s=vec.get(i).replaceAll("my|name|is| a|an|of|the|or|of|										for|in|as|at","").trim();
+											}
+											return s;
+															
+																}
